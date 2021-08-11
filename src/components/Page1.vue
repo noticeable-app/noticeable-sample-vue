24296fe7789efb06d2806770ff9986ff9fbd26d4
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2021-08-11 18:35:02 +0200

    Add Noticeable Newspage embed Widget on first page

--- src/components/Page1.vue
+++ src/components/Page1.vue
@@ -1,8 +1,20 @@
 <template>
-  <p>Page 1</p>
+  <div id="noticeable-newspage-embed" style="width: 100%"></div>
 </template>
 
 <script>
+import {config} from '../config'
+
+export default {
+  mounted() {
+    this.$nextTick(function () {
+      window.noticeable.render('widget', config.noticeable.newspageEmbedWidgetId, {selector: '#noticeable-newspage-embed'});
+    })
+  },
+  unmounted() {
+    window.noticeable.destroy('widget', config.noticeable.newspageEmbedWidgetId);
+  }
+}
 </script>
 
 <style scoped>
